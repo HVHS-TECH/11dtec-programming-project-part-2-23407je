60b15229ca01b6ffb8bd02b3239a86533967599e
23407je <23407je@hvhs.school.nz>
2025-06-10 13:21:59 +0000

    Jun 10, 2025, 1:21 PM

--- Part2.js
+++ Part2.js
@@ -107,7 +107,7 @@ if (timer2 >= 10) {
 }
     if (heropacktimer2 >= 1) {
         timer3 = timer3 + 1
-        console.log("hero second", timer3);
+        console.log("hero second");
     }
     if (timer3 >= 10) {
     heropacktimer2 = 0;
